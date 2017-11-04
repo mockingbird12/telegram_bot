7e7ac8d38a6830a9377ba904249b355306d05381
Michail <32322330+mockingbird12@users.noreply.github.com>
2017-11-04 02:26:47 -0400

    add files

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import telebot
+import conversations
+import anecdots
 
 
-TOKEN = 
+TOKEN = '411297683:AAGDRvzu9FCD0sviGuyA8GvdF5o2PGEJ660'
 bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    sent = bot.send_message(message.chat.id, 'Как тебя зовут?')
-    bot.register_next_step_handler(sent, hello)
+    sent = bot.send_message(message.chat.id, conversations.conv_bogdan('hello'))
+    # bot.register_next_step_handler(sent, hello)
 
+
+@bot.message_handler(commands=['joke'])
+def new_joke(message):
+    bot.send_message(message.chat.id, anecdots.get_new_joke())
 
 def hello(message):
     bot.send_message(message.chat.id, 'Привет, %s!' % message.text)
 
 
-bot.polling()
+if __name__ == "__main__":
+    bot.polling()
 
 
 
